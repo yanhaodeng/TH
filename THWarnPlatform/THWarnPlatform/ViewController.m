9e72aa9332a89155a7400bd6c61b5462cfb0ecab
yh <598952451@qq.com>
2018-07-06 21:23:52 +0800

    创建工程

---
+++ THWarnPlatform/THWarnPlatform/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  THWarnPlatform
+//
+//  Created by yan_hao on 2018/7/6.
+//  Copyright © 2018年 太湖局. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
